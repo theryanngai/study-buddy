2cb59de1b5265723adb9dcc808230dbf9081b422
Ryan Ngai <theryanngai@gmail.com>
2017-11-26 12:20:28 -0800

    Added express session to the mix

--- client/app/app.routing.ts
+++ client/app/app.routing.ts
@@ -4,13 +4,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { QuizCreatorComponent } from './components/quizzes/quiz-creator/quiz-creator.component';
+import { QuizCreationResultsComponent} from './components/quizzes/quiz-creation-results/quiz-creation-results.component';
 
 const routes: Routes = [
   // {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'quiz/create', component: QuizCreatorComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'quiz/create', component: QuizCreatorComponent },
+  { path: 'quiz/create/results', component: QuizCreationResultsComponent ,}
 ];
 
 @NgModule({
@@ -24,6 +26,7 @@ export const routingComponents = [
   LoginComponent,
   RegisterComponent,
   QuizCreatorComponent,
+  QuizCreationResultsComponent,
 ];
 
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const passport = require('passport');
-// const session = require('express-session');
+const session = require('express-session');
 
 
 // Get our API routes
@@ -20,11 +20,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(session({
-//   secret: process.env.SECRET_KEY,
-//   resave: false,
-//   saveUninitialized: true
-// }));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
